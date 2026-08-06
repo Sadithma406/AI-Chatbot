1dcccabeceba6a8b75e615b95f5e85c354004449
SadithmaPerera <sadithma.20242193@iit.ac.lk>
2026-08-06 21:45:29 +0530

    removing the fileReader import

--- chat_backend.py
+++ chat_backend.py
@@ -6,7 +6,6 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
 from pydantic import BaseModel
 
 # Hide TensorFlow logs
