c4ac4b8074e99311e5118cae380357ccb4291985
SadithmaPerera <sadithma.20242193@iit.ac.lk>
2026-08-14 21:42:20 +0530

    using of the USE model

--- chat_backend.py
+++ chat_backend.py
@@ -4,65 +4,40 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import os
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
 
-MODEL_PATH = "saved_model/intent_model.keras"
-CLASSES_PATH = "saved_model/classes.json"
-REPLIES_PATH = "saved_model/replies.json"
 
 USE_URL = "https://tfhub.dev/google/universal-sentence-encoder/4"
 
-CONFIDENCE_THRESHOLD = 0.25
+CONFIDENCE_THRESHOLD = 0.15
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-
 tf.get_logger().setLevel("ERROR")
 
 print("Loading Universal Sentence Encoder...")
-
 use = hub.load(USE_URL)
-
 print("Universal Sentence Encoder loaded successfully.")
 
 print("Loading intent classification model...")
-
-model = tf.keras.models.load_model( MODEL_PATH,compile=False)
-
+model = tf.keras.models.load_model( "saved_model/intent_model.keras",compile=False)
 print("Intent model loaded successfully.")
 
-with open(CLASSES_PATH,"r",encoding="utf-8") as f:
+with open("saved_model/classes.json","r",encoding="utf-8") as f:
     intent_classes = json.load(f)
 
 
-with open(REPLIES_PATH,"r",encoding="utf-8") as f:
+with open("saved_model/replies.json","r",encoding="utf-8") as f:
     replies = json.load(f)
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-class ChatRequest(BaseModel):
-    message: str
 
 @app.post("/api/chat")
-def chat(request: ChatRequest):
-    user_input = request.message.strip().lower()
-    # Empty message
-    if not user_input:
-        return {
-            "intent": "unknown",
-            "reply": "Please enter a message.",
-            "confidence": 0
-  }
+async def chat(request: Request):
+    data = await request.json()
+    user_input = str(data.get("message", "")).strip().lower()
+
     embedding = use([user_input])
     embedding = embedding.numpy().astype(np.float32)
     prediction = model.predict(embedding,verbose=0)
@@ -77,19 +52,8 @@ def chat(request: ChatRequest):
             "Could you please rephrase?"
         )
     else:
-        reply = replies.get(
-            predicted_intent,
-            {}
-        ).get(
-            "reply",
-            "Sorry, I don't understand that."
-        )
+        reply = replies.get(predicted_intent,{}).get("reply","Sorry, I don't understand that.")
     return {
-        "intent": predicted_intent,
         "reply": reply,
-        "confidence": round(
-            confidence * 100,
-            2
-        )
     }
 app.mount("/",StaticFiles(directory="public",html=True),name="public")
